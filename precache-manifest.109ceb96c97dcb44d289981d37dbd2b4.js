7ea51af0493c386312faf4bfba72d70c2798234e
byeongukk <cbuk92@gmail.com>
2019-09-27 17:36:42 +0900

    Updates

---
+++ precache-manifest.109ceb96c97dcb44d289981d37dbd2b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a75834cbc49f5a4d90f2a1b9edf72b4e",
+    "url": "/test_app/index.html"
+  },
+  {
+    "revision": "246b19842dcb193672c7",
+    "url": "/test_app/static/css/main.2eb6c513.chunk.css"
+  },
+  {
+    "revision": "d9e5c03166eff5b1d2e6",
+    "url": "/test_app/static/js/2.c3c1a6e4.chunk.js"
+  },
+  {
+    "revision": "246b19842dcb193672c7",
+    "url": "/test_app/static/js/main.7333de36.chunk.js"
+  },
+  {
+    "revision": "33a6a63ce5adc5546675",
+    "url": "/test_app/static/js/runtime-main.76e9580a.js"
+  }
+]);
